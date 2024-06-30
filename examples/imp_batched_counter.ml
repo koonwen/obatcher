64f3d5379dc94c6fa15fe737da71317158426874
Lee Koon Wen <koonwen@gmail.com>
2024-06-30 15:58:59 +0200

    initial work on data structures & examples

---
+++ examples/imp_batched_counter.ml
@@ -0,0 +1,45 @@
+open Picos
+
+module BatchedCounter = struct
+  type t = int Atomic.t
+
+  let init ~ctx:_ = Atomic.make 0
+
+  type _ op = Incr : unit op | Decr : unit op | Get : int op
+  type wrapped_op = Mk : 'a op * 'a Computation.t -> wrapped_op
+
+  (* Internal parallelism in counter *)
+  let run (t : t) (ops : wrapped_op array) =
+    let start = Atomic.get t in
+    let thunks =
+      Array.mapi
+        (fun i op ->
+          match op with
+          | Mk (Incr, comp) ->
+              fun () ->
+                Printf.printf "running Incr slot %d\n%!" i;
+                Unix.sleepf 1.0;
+                Atomic.incr t;
+                Computation.return comp ()
+          | Mk (Decr, comp) ->
+              fun () ->
+                Printf.printf "running Decr slot %d\n%!" i;
+                Unix.sleepf 1.0;
+                Atomic.decr t;
+                Computation.return comp ()
+          | Mk (Get, comp) ->
+              fun () ->
+                Printf.printf "running Get slot %d\n%!" i;
+                Unix.sleepf 1.0;
+                Computation.return comp start)
+        ops
+    in
+    Picos_structured.Run.all (thunks |> Array.to_list)
+end
+
+(* Set up implicit batching *)
+include Obatcher.Make (BatchedCounter)
+
+let incr t = apply t Incr
+let decr t = apply t Decr
+let get t = apply t Get
