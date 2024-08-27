43cf671c59119907adcba18f637e926d644f17c0
koonwen <koonwen@gmail.com>
2024-08-27 11:11:47 +0200

    Prevent pop when element hasn't made it to queue

--- lib/ts_container.ml
+++ lib/ts_container.ml
@@ -5,14 +5,16 @@ let create ?(batch_limit = max_int) () =
   { q = Picos_mpscq.create (); size = Atomic.make 0; batch_limit }
 
 let add t elt =
-  Atomic.incr t.size;
-  Picos_mpscq.push t.q elt
+  Picos_mpscq.push t.q elt;
+  Atomic.incr t.size
 
 let get t =
   let batch_size = Atomic.exchange t.size 0 in
   let limit = min batch_size t.batch_limit in
   let topup = batch_size - limit in
   let _ = Atomic.fetch_and_add t.size topup in
-  Array.init limit (fun _ -> Picos_mpscq.pop_exn t.q)
+  (* TODO: We alllocte each time, this is bad, should reuse the *)
+  (*    array *)
+  Array.init batch_size (fun _ -> Picos_mpscq.pop_exn t.q)
 
 let size t = Atomic.get t.size
