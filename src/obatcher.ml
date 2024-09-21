0126a6be271ae754bebe3b89fbc345ac38cc26bc
Koonwen Lee <koonwen.lee@ahrefs.com>
2024-09-21 09:11:04 +0800

    obatcher: add polymorphic signature and Make functor

--- src/obatcher.ml
+++ src/obatcher.ml
@@ -48,3 +48,54 @@ module Make (S : Service) = struct
 
   let get_internal t = t.internal
 end
+
+module type Service_Poly = sig
+  type 'a t
+  type cfg
+  type ('a, 'b) op
+  type 'a wrapped_op = Mk : ('a, 'b) op * 'b Picos.Computation.t -> 'a wrapped_op
+
+  val init : ?cfg:cfg -> unit -> 'a t
+  val run : 'a t -> 'a wrapped_op array -> unit
+end
+
+module Make_Poly (S : Service_Poly) = struct
+  type 'a t = {
+    internal : 'a S.t;
+    running : bool Atomic.t;
+    container : 'a S.wrapped_op Ts_container.t;
+  }
+
+  let init ?cfg () =
+    {
+      internal = S.init ?cfg ();
+      running = Atomic.make false;
+      container = Ts_container.create ();
+    }
+
+  (* TODO: This currently does busy polling by rescheduling threads
+     the run at a later time, ideally we should just do something like
+     a backoff wait instead *)
+  let exec t op =
+    let open Picos in
+    let comp = Computation.create () in
+    let op_set = S.Mk (op, comp) in
+    Ts_container.add t.container op_set;
+    (* Try launching batch *)
+    while Computation.peek comp = None do
+      if
+        Ts_container.size t.container > 0
+        && Atomic.compare_and_set t.running false true
+      then (
+        (* Batching Fiber *)
+        let batch = Ts_container.get t.container in
+        S.run t.internal batch;
+        Atomic.set t.running false)
+      else
+        (* A batch is being processed, yield and try again later *)
+        Fiber.yield ()
+    done;
+    Computation.await comp
+
+  let get_internal t = t.internal
+end
--- src/obatcher.mli
+++ src/obatcher.mli
@@ -51,3 +51,57 @@ module Make : functor (S : Service) -> sig
   [@@alert unsafe "Should not be used typically by application"]
   (** [get_internal t] extracts underlying internal service *)
 end
+
+module type Service_Poly = sig
+  type 'a t
+  (** [t] represents the underlying service that processes and handles
+      batches of operations as input. Only a single batch is active at
+      any time *)
+
+  type cfg
+  (** [cfg] represents config variables that you want to expose
+      to users. *)
+
+  type ('a, 'b) op
+  (** ['a op] represents an single operation on [t] with the return
+      type ['a]. *)
+
+  type 'a wrapped_op =
+    | Mk : ('a, 'b) op * 'b Picos.Computation.t -> 'a wrapped_op
+        (** [wrapped_op] binds the operation on the service with it's
+        corresponding suspended continuation to run after its
+        completion.  *)
+
+  (* Add max_batsz parameter *)
+  (* val max_batch_sz : int option *)
+  (* (\** Configure the maximum batch size that the service will handle at *)
+  (*     any time *\) *)
+
+  val init : ?cfg:cfg -> unit -> 'a t
+  (** [init ()] returns a new instance of the service. *)
+
+  val run : 'a t -> 'a wrapped_op array -> unit
+  (** [run t ops] when called on service [t], processes all the
+      operations in [ops], possibly using parallelism to complete the
+      batch. *)
+end
+
+module Make_Poly : functor (S : Service_Poly) -> sig
+  type 'a t
+  (** [t] represents the service now abstracted with batching made
+      implicit & automatic *)
+
+  (* May be useful to add a timeout parameter if requests don't need to be handled immediately *)
+  val init : ?cfg:S.cfg -> unit -> 'a t
+  (** [init ?cfg ()] initializes service with implicit batching. [cfg]
+      is passed on the service initialization *)
+
+  val exec : 'a t -> ('a, 'b) S.op -> 'b
+  (** [exec t op] is the API call for a singular operation on the
+      service with operations being automatically batched before
+      passed to the service *)
+
+  val get_internal : 'a t -> 'a S.t
+  [@@alert unsafe "Should not be used typically by application"]
+  (** [get_internal t] extracts underlying internal service *)
+end
